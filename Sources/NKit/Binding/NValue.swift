d8d8e67de5ebc71afd29b9645e1f895ba642c737
Mateusz Madej <madejmt@gmail.com>
2025-08-05 09:51:42 +0200

    Update NValue

--- Sources/NKit/Binding/NValue.swift
+++ Sources/NKit/Binding/NValue.swift
@@ -4,6 +4,8 @@ import Foundation
 public final class NValue<Value> {
     public var wrappedValue: Value
     
+    public var projectedValue: NValue<Value> { self }
+    
     public init(wrappedValue: Value) {
         self.wrappedValue = wrappedValue
     }
