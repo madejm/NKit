0c1e14e61c450e0df2b49faac4eee2e4dbe489c5
Mateusz Madej <madejmt@gmail.com>
2025-09-03 01:59:40 +0200

    Add debugging logging flag

--- Sources/NKit/Debug/print_debug.swift
+++ Sources/NKit/Debug/print_debug.swift
@@ -5,11 +5,15 @@
 //  Created by Mejdej on 13/08/2025.
 //
 
+nonisolated(unsafe) public var NKitDebugLoggingEnabled: Bool = false
+
 package func print_debug(
     _ message1: @autoclosure () -> Any
 ) {
     #if DEBUG
-    print(message1())
+    if NKitDebugLoggingEnabled {
+        print(message1())
+    }
     #endif
 }
 
@@ -18,7 +22,9 @@ package func print_debug(
     _ message2: @autoclosure () -> Any
 ) {
     #if DEBUG
-    print(message1(), message2())
+    if NKitDebugLoggingEnabled {
+        print(message1(), message2())
+    }
     #endif
 }
 
@@ -28,7 +34,9 @@ package func print_debug(
     _ message3: @autoclosure () -> Any
 ) {
     #if DEBUG
-    print(message1(), message2(), message3())
+    if NKitDebugLoggingEnabled {
+        print(message1(), message2(), message3())
+    }
     #endif
 }
 
@@ -39,7 +47,9 @@ package func print_debug(
     _ message4: @autoclosure () -> Any
 ) {
     #if DEBUG
-    print(message1(), message2(), message3(), message4())
+    if NKitDebugLoggingEnabled {
+        print(message1(), message2(), message3(), message4())
+    }
     #endif
 }
 
@@ -51,6 +61,8 @@ package func print_debug(
     _ message5: @autoclosure () -> Any
 ) {
     #if DEBUG
-    print(message1(), message2(), message3(), message4(), message5())
+    if NKitDebugLoggingEnabled {
+        print(message1(), message2(), message3(), message4(), message5())
+    }
     #endif
 }
